88b29a6221272439d31c6ccb7dd4212873e3b9bb
bilyos <melimerveil@gmail.com>
2024-05-09 15:27:56 +0100

    lot 2

--- exo.js
+++ exo.js
@@ -54,3 +54,170 @@ export const fetchData =()=> {
     });
   }
 
+
+
+
+  //------------------------------- LOT 2 ---------------------------------------
+
+
+
+// ----------- EXERCICE 1 -----------------
+
+const input = document.getElementById("usernumber");
+const nombreAleatoire = Math.floor(Math.random() * 10) + 1;
+
+input.addEventListener("input", () => {
+  const valeuruser = parseInt(input.value);
+  if (valeuruser === nombreAleatoire) {
+    alert("Bon travail");
+  } else {
+    alert("Ne correspond pas");
+  }
+});
+
+//---------- EXERCICE 2 ---------------
+export const nextChrismas = () => {
+  const dateActuelle = new Date();
+  const anneeActuel = dateActuelle.getFullYear();
+  const prochainNoel = new Date(anneeActuel, 11, 25);
+  const unjourEnSeconde = 24 * 60 * 60;
+  const diffenSeconde = prochainNoel - dateActuelle;
+  const jourRestant = Math.ceil(diffenSeconde / unjourEnSeconde);
+
+  return jourRestant;
+};
+
+//------------- EXERCICE 3 -----------------
+export const mulDiv = (num1, num2) => {
+  if (num1 && num2 > 0) {
+    return `la mutiliplication des 2 nombre est : ${
+      num1 * num2
+    } et la div est: ${num1 / num2}`;
+  } else if (num1 === undefined || num2 === undefined) {
+    return "Les deux nombres doivent être définis.";
+  } else {
+    return "Les deux nombres doivent être supérieurs à zéro.";
+  }
+};
+
+
+//-------------- EXERCICE 4 ----------------
+
+ export const longuestString = (tableau) => {
+  let laplusLongueChaine = "";
+  tableau.foreach((item) => {
+    if (item.length > laplusLongueChaine.length) {
+      laplusLongueChaine = item;
+    }
+  });
+  return laplusLongueChaine;
+};
+
+//----------- EXERCICE 5 ------------------
+ export const largestEvenNumber = (tableau) => {
+  let leplusgrdNombrePaire = 0;
+  tableau.foreach((item) => {
+    if (item % 2 === 0 && item > leplusgrdNombrePaire) {
+      leplusgrdNombrePaire = item;
+    }
+  });
+  return leplusgrdNombrePaire;
+};
+
+//------------- EXECICE 6 ------------
+ export const removeCharacter = (chaine) => {
+  let nbreOcc = {};
+  for (let i = 0; i < chaine.length; i++) {
+    const character = chaine[i];
+    nbreOcc[character] = (nbreOcc[character] || 0) + 1;
+  }
+  const resultat = chaine
+    .split("")
+    .filter((character) => nbreOcc[character] === 1)
+    .join("");
+  return resultat;
+};
+
+//---------- EXERCICE 7 -------------
+export const cubenumber = (nombre) => {
+  let sum = 0;
+  for (let i = 1; i < nombre; i++) {
+    sum += Math.pow(nombre, 3);
+  }
+  return sum;
+};
+
+//---------- EXERCICE 8-------------------
+export const compare = (obj1, obj2) => {
+  return obj1.property.value === obj2.property.value;
+};
+
+
+//------------ EXERCICE 9 ---------------
+
+ export const functionFilter = (tableau, valeursfiltrer) => {
+  return tableau.filter((item) => !valeursfiltrer.includes(item));
+};
+
+//----------- EXERCICE 10 --------------------
+export const functionExtract = (tableau, valeuraSupp) => {
+  for (let i = 0; i < tableau.length; i++) {
+    if (tableau[i] === valeuraSupp) {
+      tableau.splice(i, 1);
+      i--; 
+    }
+  }
+};
+
+
+//------------ EXERCICE 11---------------------
+
+const student = {
+  ame: "David Rayy",
+  sclass: "VI",
+  rollno: 12,
+};
+console.log(student);
+delete student.rollno;
+console.log(student);
+
+//--------- EXERCICE 12 -------------------
+
+class volume {
+  constructor(radius, height) {
+    this.radius = radius;
+    this.height = height;
+  }
+
+  calculerVolume() {
+    return Math.PI * Math.pow(this.radius, 2) * this.height;
+  }
+}
+
+//-------------- EXERCICE 13 ---------------
+
+ export const createClock = () => {
+  const nouvelDate = new Date();
+  const heure = nouvelDate.getHours;
+  const minute = nouvelDate.getMinutes;
+  const seconde = nouvelDate.getSeconds;
+  return (`${heure}:${minute}:${seconde}`);
+};
+
+//--------------- EXERCICE 14 ----------------
+ export const lowerCaseFunction = (chaine) => {
+  return chaine === chaine.toLowerCase();
+};
+
+//--------------- EXERCICE 15 -----------------
+ export const add = (param1, param2) => {
+  return new Promise((resolve, reject) => {
+    if (param1 === undefined || param2 === undefined) {
+      reject("doit entrer les paramètres");
+    } else {
+      const result = param1 + param2;
+      resolve(result);
+    }
+  });
+};
+
--- exo.test.js
+++ exo.test.js
@@ -15,18 +15,13 @@ test("trouver le grand nombre dans un tableau", ()=>{
 //---------- Exercice 3 ------------
 
 test("Générer une facture avec les bonnes informations", () => {
-    const quantite = 3;
-    const produit = "Ordinateur portable";
-    const prixUnitaire = 1200;
-    const resultatAttendu = "Vous avez acheté 3 unités de Ordinateur portable pour un total de $3600";
-    expect(genererFacture(quantite, produit, prixUnitaire)).toBe(resultatAttendu);
+   
+    expect(genererFacture(quantite, produit, prixUnitaire)).toBe("Vous avez acheté 3 unités de Ordinateur portable pour un total de $3600");
 });
 
 // --------- Exercice 4 --------------
 
 test("verifier la fusion de deux tableaux", ()=>{
-    const tab1 = [2, 4, 5]
-    const tab2 = [9, 7, 0, 8]
     const tableauCombine = [...tab1, ...tab2];
     expect(tableauCombine).toEqual([2, 4, 5, 9, 7, 0, 8])
 })
@@ -51,9 +46,8 @@ test("Renvoie un objet avec les valeurs fournies", () => {
   
 //---------- Exercice 6---------
 test("Calculer correctement l'aire d'un cercle", () => {
-    const rayon = 5;
-    const area =  5 * 5 * Math.PI; 
-    expect(calculerArea(rayon)).toBe(area);
+
+    expect(calculerArea(5)).toBe(5 * 5 * Math.PI);
 });
 
 test("Gérer correctement le cas où le rayon est manquant", () => {
@@ -78,12 +72,7 @@ test("inverser l'ordre des elt dans un tableau", ()=>{
 // --------- Exercice 9 ----------
 
 test("Filtrer les livres par nombre de pages", () => {
-    const livres = [
-        { titre: "jeune millionnaire", page: 250 },
-        { titre: "40 ans de prison", page: 310 },
-        { titre: "Reflechir", page: 300 },
-        { titre: "48 loi du pouvoir", page: 500 },
-    ];
+    
     const resultatAttendu = [
         { titre: "40 ans de prison", page: 310 },
         { titre: "48 loi du pouvoir", page: 500 },
@@ -91,4 +80,113 @@ test("Filtrer les livres par nombre de pages", () => {
     expect(filtrerParPage(livres)).toEqual(resultatAttendu);
 });
 
-//------------ Exercice 10 ------------
+
+//--------------------------- LOT 2 ------------------------------------------
+
+
+
+const {mulDiv, longuestString} = require('./script') 
+//---------- EXERCICE 3 -------------
+test('Retourne la multiplication et la division des deux nombres si les deux sont supérieurs à zéro', () => {
+    expect(mulDiv(4, 2)).toBe('la multiplication des 2 nombres est : 8 et la division est : 2');
+  });
+
+  test('Retourne "Les deux nombres doivent être définis." si l\'un des deux nombres est indéfini', () => {
+    expect(mulDiv(4)).toBe('Les deux nombres doivent être définis.');
+  });
+
+  test('Retourne "Les deux nombres doivent être supérieurs à zéro." si l\'un des deux nombres est inférieur ou égal à zéro', () => {
+    expect(mulDiv(4, 0)).toBe('Les deux nombres doivent être supérieurs à zéro.');
+    expect(mulDiv(0, 4)).toBe('Les deux nombres doivent être supérieurs à zéro.');
+  });
+
+//------------ EXERCICE 4 ------------
+
+test('Retourne la chaîne la plus longue dans le tableau', () => {
+    expect(longuestString( ['chat', 'chien', 'éléphant', 'souris'])).toBe('éléphant');
+  });
+
+  test('Retourne une chaîne vide si le tableau est vide', () => {
+    const tableau = [];
+    expect(longuestString(tableau)).toBe('');
+  });
+
+  //--------------- EXERCICE 5 ----------------
+
+  test('Retourne le plus grand nombre pair dans un tableau', () => {
+    const tableau = [1, 2, 3, 4, 5, 6];
+    expect(largestEvenNumber(tableau)).toBe(6);
+  });
+
+  test('Retourne 0 si aucun nombre pair n\'est présent dans le tableau', () => {
+    const tableau = [1, 3, 5, 7, 9];
+    expect(largestEvenNumber(tableau)).toBe(0);
+  });
+  //--------------- EXERCICE 6 ------------------
+
+  test('Retourne une chaîne sans caractères en double', () => {
+    const chaine = 'bonjour';
+    expect(removeCharacter(chaine)).toBe('bnu');
+  });
+
+  test('Retourne la même chaîne si aucun caractère n\'est répété', () => {
+    const chaine = 'hello';
+    expect(removeCharacter(chaine)).toBe(chaine);
+  });
+
+//--------------- EXERCICE 7 ----------
+
+test('Retourne la somme des cubes des entiers jusqu\'à nombre', () => {
+  expect(cubenumber(3)).toBe(36); 
+});
+
+test('Retourne 0 si nombre est 0', () => {
+  expect(cubenumber(0)).toBe(0);
+});
+
+//-------------- EXERCICE 8 ----------------
+
+test('Retourne true si les valeurs des propriétés sont égales', () => {
+  const obj1 = { property: { value: 5 } };
+  const obj2 = { property: { value: 5 } };
+  expect(compare(obj1, obj2)).toBe(true);
+});
+
+test('Retourne false si les valeurs des propriétés sont différentes', () => {
+  const obj1 = { property: { value: 5 } };
+  const obj2 = { property: { value: 10 } };
+  expect(compare(obj1, obj2)).toBe(false);
+});
+
+//----------- EXERCICE 9 -------------
+
+test('Retourne un tableau filtré en fonction des valeurs à filtrer', () => {
+  const tableau = [1, 2, 3, 4, 5];
+  const valeursfiltrer = [2, 4];
+  expect(functionFilter(tableau, valeursfiltrer)).toEqual([1, 3, 5]);
+});
+
+//------------ EXERCICE 10 -------------
+
+test('Retire une valeur du tableau si elle est présente', () => {
+  expect(functionExtract([1, 2, 3, 4, 5], 3)).toEqual([1, 2, 4, 5]);
+});
+
+//----------- EXERCICE 12 ------------
+test('Calcul du volume pour un cylindre de rayon 2 et de hauteur 5', () => {
+  const cylindre = new Volume(2, 5);
+  expect(cylindre.calculerVolume()).toBeCloseTo(62.83);
+});
+
+//----------- EXERCICE 13 -------------
+
+
+//--------------- EXERCICE 14 --------------
+
+test('Retourne true si la chaîne est entièrement en minuscules', () => {
+  expect(lowerCaseFunction('hello')).toBe(true);
+});
+
+test('Retourne false si la chaîne contient des lettres majuscules', () => {
+  expect(lowerCaseFunction('Hello')).toBe(false);
+});
